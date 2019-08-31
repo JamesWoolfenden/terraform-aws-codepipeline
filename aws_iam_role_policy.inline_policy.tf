99adee6575c0b60d2320641cb83f7b7952fa41d1
James Woolfenden <James.Woolfenden@gmail.com>
2019-08-31 20:12:35 +0100

    test of auto vars for tf cloud

--- aws_iam_role_policy.inline_policy.tf
+++ aws_iam_role_policy.inline_policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy" "inline_policy" {
-  count = "${var.role_arn == "" ? 1 : 0}"
+  count = var.role_arn == "" ? 1 : 0
   name  = "AWSCodePipeline-${data.aws_region.current.name}-${var.name}"
   role  = aws_iam_role.pipeline.0.name
 
