4358a70d0dc682dc61995dbef63bb9d0e519fb56
James Woolfenden <james.woolfenden@gmail.com>
2021-03-08 09:57:03 +0000

    Managed by Terraform

--- validate.sh
+++ validate.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+terraform fmt --recursive
 cd example/examplea || return
-rm .terraform.lock.hcl
-rm .terraform -fr
+rm .terraform.lock.hcl 2>/dev/null
+rm .terraform -fr 2>/dev/null
 terraform init -upgrade
 terraform validate
 make valid
