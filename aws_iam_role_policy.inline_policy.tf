0e6fcc5217d83ecf8a34c42341d8f01e059cd0cb
Francois Laupretre <francois.laupretre@ontexglobal.com>
2021-03-15 18:20:20 +0000

    Add 'role_name' output variable

--- aws_iam_role_policy.inline_policy.tf
+++ aws_iam_role_policy.inline_policy.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy" "inline_policy" {
   count = var.role_arn == "" ? 1 : 0
-  name  = "AWSCodePipeline-${data.aws_region.current.name}-${var.name}"
+  name  = local.role_name
   role  = aws_iam_role.pipeline.0.name
 
   policy = data.aws_iam_policy_document.pipeline.json
