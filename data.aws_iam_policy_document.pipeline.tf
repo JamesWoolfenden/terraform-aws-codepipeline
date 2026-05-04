876a4b2a4bcf9e40d09e462e0624cc1d56fe8658
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:29:39 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- data.aws_iam_policy_document.pipeline.tf
+++ data.aws_iam_policy_document.pipeline.tf
@@ -1,4 +1,6 @@
 data "aws_iam_policy_document" "pipeline" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   # checkov:skip=CKV_AWS_107: Codepipeline not in prod
   # checkov:skip=CKV_AWS_108
   # checkov:skip=CKV_AWS_109
